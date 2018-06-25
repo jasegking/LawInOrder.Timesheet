a05ffbf900163a54641926092e51f47fce171e7a
Jase King <jase.g.king@gmail.com>
2018-06-25 19:58:07 +1000

    Add project files.

---
+++ LawInOrder.Timesheet.Web/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace LawInOrder.Timesheet.Web
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ LawInOrder.Timesheet.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LawInOrder.Timesheet.Web.MvcApplication" Language="C#" %>
